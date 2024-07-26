a7dac14c8bdb2bf5e9e57c8baecffc4a38baec7b
npanis <neriza.montes@gmail.com>
2024-07-26 16:32:35 +0900

    Updated example

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
+const sayHello = (req, res, next) => {
+    res.send("Olla!");
+  };
+  const logging = (req, res, next) => {
+    console.log("A request is being made!");
+    next();
+  };
+
+app.use(logging);
+app.use(sayHello);
 
 module.exports = app;
